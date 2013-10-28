9860dcd375a55ccfbca268c8aac7fa9fd99c7270
Jack Polgar <jack@polgar.id.au>
2013-10-29 02:24:01 +1100

    Updated tests.

--- test.php
+++ test.php
@@ -2,27 +2,43 @@
 require 'Phails/init.php';
 require 'PhailSafe/init.php';
 
-function capture_puts($content)
+function capture_puts($content, $replacements = [])
 {
     ob_start();
-    puts($content);
+    puts($content, $replacements);
     return ob_get_clean();
 }
 
-test("should print string", function($test) {
-    $test_string = str("Hello, world");
-    $test->shouldEqual("Hello, world", capture_puts($test_string));
+test_group("Str class", function (){
+    test("should print string", function ($test) {
+        $test_string = str("Hello, world");
+        $test->shouldEqual("Hello, world", capture_puts($test_string));
+    });
+
+    test("should split string and print array", function ($test) {
+        $test_array = str("Hello, world")->split(', ');
+        $test->shouldEqual('["Hello","world"]', capture_puts($test_array));
+    });
+
+    test("should return true for string->includes()", function ($test) {
+        $test->assertTrue(str('Test string')->includes('ing'));
+    });
+
+    test("should replace {something}", function ($test) {
+        $test_string = str("Something in the {something} tonight")->replace('{something}', 'air');
+        $test->shouldEqual('Something in the air tonight', $test_string->toString());
+    });
 });
 
-test("should split string and print array", function($test){
-    $test_array = str("Hello, world")->split(', ');
-    $test->shouldEqual('["Hello","world"]', capture_puts($test_array));
+test_group("Arr class", function () {
+    test("should return true for array->includes()", function ($test) {
+        $test->assertTrue(arr(['test', 'array'])->includes('test'));
+    });
 });
 
-test("should return true for string->includes()", function($test){
-    $test->assertTrue(str('Test string')->includes('ing'));
-});
-
-test("should return true for array->includes()", function($test){
-    $test->assertTrue(arr(['test', 'array'])->includes('test'));
+test_group('puts() function', function () {
+    test("should replace {something}", function ($test) {
+        $test_string = str("Something in the {something} tonight");
+        $test->shouldEqual('Something in the air tonight', capture_puts($test_string, arr(['something' => 'air'])));
+    });
 });
