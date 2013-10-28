e6c86074c53f7848bf14fdc3f7a083c52b7d1f1b
Jack Polgar <jack@polgar.id.au>
2013-10-28 21:48:43 +1100

    Tests started.

---
+++ test.php
@@ -0,0 +1,20 @@
+<?php
+require 'Phpp/init.php';
+require 'PhpTest/init.php';
+
+function capture_puts($content)
+{
+    ob_start();
+    puts($content);
+    return ob_get_clean();
+}
+
+test("should print string", function($test) {
+    $test_string = str("Hello, world");
+    $test->shouldEqual("Hello, world", capture_puts($test_string));
+});
+
+test("should split string and print array", function($test){
+    $test_array = str("Hello, world")->split(', ');
+    $test->shouldEqual('["Hello","world"]', capture_puts($test_array));
+});
